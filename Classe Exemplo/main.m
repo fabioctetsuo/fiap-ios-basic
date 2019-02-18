7f0c0d74b7e5e3fe67d767a35b6abce3bff80fdb
Usuário Convidado <Guest@l2602micro12.local>
2019-02-18 19:56:14 -0300

    Initial Commit

---
+++ Classe Exemplo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Classe Exemplo
+//
+//  Created by Usuário Convidado on 18/02/19.
+//  Copyright © 2019 Usuário Convidado. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
